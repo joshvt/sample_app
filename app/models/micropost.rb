bb80391be9f52a02055980e6de087b70e32a4abc
Josh van Tonder <joshvt@gmail.com>
2012-05-26 16:40:15 -0700

    Add user following

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,7 +3,13 @@ class Micropost < ActiveRecord::Base
   validates  :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
   belongs_to :user
+
   default_scope order: 'microposts.created_at DESC'
 
-
+  def self.from_users_followed_by(user)
+    select_followed_user_ids = "SELECT followed_id FROM relationships
+                         WHERE follower_id = :user_id"
+    where("user_id IN (#{select_followed_user_ids}) OR user_id = :user_id", 
+          user_id: user.id)
+  end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -82,6 +82,18 @@ describe "AuthenticationPages" do
           end
           specify { response.should redirect_to(signin_path) }
         end
+
+        describe "in the Relationships controller" do
+          describe "submitting to the create action" do
+            before { post relationships_path }
+            specify { response.should redirect_to(signin_path) }
+          end
+
+          describe "submitting to the destroy action" do
+            before { delete relationship_path(1) }
+            specify { response.should redirect_to(signin_path) }          
+          end
+        end        
       end
 
       describe "when attempting to visit a protected page" do
@@ -114,6 +126,16 @@ describe "AuthenticationPages" do
         describe "visiting the user index" do
           before {visit users_path}
           it  {should have_selector('title', text: "Sign in")}
+        end
+
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
         end
       end
 
