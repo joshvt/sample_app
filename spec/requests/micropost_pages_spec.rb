b6ca09f5376fc325ccfcaf5f122f317cd531f7e9
Josh van Tonder <joshvt@gmail.com>
2012-05-25 19:22:28 -0700

    Add exercise 10 fixes

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -25,9 +25,11 @@ describe "Micropost pages" do
     describe "with valid information" do
 
       before { fill_in 'micropost_content', with: "Lorem ipsum" }
+      
       it "should create a micropost" do
         expect { click_button "Post" }.should change(Micropost, :count).by(1)
       end
+      it { should have_content(Micropost.count.to_s + ' microposts') }
     end
   end
 
@@ -39,6 +41,31 @@ describe "Micropost pages" do
 
       it "should delete a micropost" do
         expect { click_link "delete" }.should change(Micropost, :count).by(-1)
+      end
+    end
+
+    describe "as incorrect user" do
+      before do
+       other_user = FactoryGirl.create(:user) 
+       visit user_path(other_user)
+      end
+
+      it "should not have delete micropost link" do
+        page.should_not have_link("delete") 
+      end
+    end
+  end
+
+  describe "micropose pagination" do
+    before (:all) { 31.times { FactoryGirl.create(:micropost, user: user) } }
+    after (:all)  { user.microposts.delete_all }
+
+    it { should have_link('Next') }
+    its(:html) { should match('>2</a>') }
+
+    it "should list each micropost" do
+      user.microposts.all[0..2].each do |mp|
+        page.should have_selector('li', text: mp.content)
       end
     end
   end
